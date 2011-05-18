bd890d4476aa90b3b7c59feae462edebd28db325
Josh McArthur <joshua.mcarthur@gmail.com>
2011-05-19 11:28:33 +1200

    Update mailer to use attachments[] instead of attachment[]

--- lib/import_products/user_mailer_ext.rb
+++ lib/import_products/user_mailer_ext.rb
@@ -5,7 +5,7 @@ module ImportProducts
         def product_import_results(user, error_message = nil)
           @user = user
           @error_message = error_message
-          attachment["import_products.log"] = File.read(ImportProductSettings::LOGFILE) if @error_message.nil?
+          attachments["import_products.log"] = File.read(IMPORT_PRODUCT_SETTINGS[:log_to]) if @error_message.nil?
           mail(:to => @user.email, :subject => "Spree: Import Products #{error_message.nil? ? "Success" : "Failure"}")
         end     
       end
